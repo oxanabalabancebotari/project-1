7806619a833d22c9a88e0289aa9de738e54fe998
oxanabalabancebotari <42274858+oxanabalabancebotari@users.noreply.github.com>
2018-10-29 23:17:39 -0400

    Add files via upload

--- Bagels/Tests/Test.spec.js
+++ Bagels/Tests/Test.spec.js
@@ -2,4 +2,7 @@ describe('Test', () => {
     it('Should save the changes', () => {
         browser.get(title)
     });
-}); 
+    it('Should merge the code', () => {
+    browser.sleep(3000)
+    });
+});
